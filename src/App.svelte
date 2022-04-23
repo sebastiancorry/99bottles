509df6106dfccc700b0cbafe860f5fbbc6d54dff
Sebastian Corry <sebastian.corry@outlook.com>
2022-04-22 22:58:48 -0500

    Button components created.

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,48 @@
 <script>
+    // Imports
+    import Button from './Button.svelte';
+
+    // State
+    let state = 'stock';
+    function setSales() {
+        state = 'sales';
+    }
+    function setStock() {
+        state = 'stock';
+    }
 </script>
 
-<main>
-</main>
+<div class="main-content">
+    <Button on:click="setStock">
+        <p>Stock</p>
+    </Button>
+
+    <Button on:click="setStock">
+        <p>Sales</p>
+    </Button>
+</div>
 
 <style>
+    .main-content{
+        /* Colors */
+        background-color: #FFFFF0;
+
+        /* Miscellaneous properties */
+        overflow: auto;
+
+        /* Dimensions */
+        min-height: 95vh;
+        max-height: 100vh;
+        max-width: 80vw;
+
+        /* Rounded corners */
+        border-radius: 5px;
+        padding: 20px;
+
+        /* Position */
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+    }
 </style>
---
+++ src/Button.svelte
@@ -0,0 +1,13 @@
+<button on:click>
+    <slot />
+</button>
+
+<style>
+    button {
+        background-color: #333333;
+        color: #FFFFF0;
+
+        border-radius: 4px;
+        padding: 20px;
+    }
+</style>
